44dc5cd2d3df3411010db46f68fbbc43d585ac55
Arturo Murillo <arturo.murillo@rappi.com>
2018-01-11 13:18:02 -0500

    add reader to RTF File

--- WordCounter/ViewController.swift
+++ WordCounter/ViewController.swift
@@ -13,6 +13,36 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        guard let filepath = Bundle.main.url(forResource: "speeches", withExtension: "json"), let data = try? Data(contentsOf: filepath) else {
+            return
+        }
+        
+//        do {
+//            let json = try JSONSerialization.jsonObject(with: data, options: []) as! [String: AnyObject]
+//
+//        } catch let error as NSError {
+//            print("Failed to load: \(error.localizedDescription)")
+//        }
+        do {
+            let string: NSAttributedString = try NSAttributedString(data: data, options: [NSAttributedString.DocumentReadingOptionKey.documentType: NSAttributedString.DocumentType.rtf], documentAttributes: nil)
+            
+            let attributedStringWithRtf: NSAttributedString = try NSAttributedString(url: filepath, options: [NSAttributedString.DocumentReadingOptionKey.documentType: NSAttributedString.DocumentType.rtf], documentAttributes: nil)
+            
+            print(attributedStringWithRtf)
+            
+            let plainString = attributedStringWithRtf.string
+            let jsonData = plainString.data(using: String.Encoding.utf8)
+            
+            let json = try JSONSerialization.jsonObject(with: jsonData!, options: []) as? [String: Any]
+
+            print("PRUBA IT WORKED")
+            
+            
+            
+        } catch let error {
+            print("Got an error \(error)")
+        }
     }
 
     override func didReceiveMemoryWarning() {
